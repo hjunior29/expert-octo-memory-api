84639b9ce2953e24dbdd6de182e516453ad18537
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-03-04 23:01:20 -0300

    Add folders and flashcards tables with relationships to users and folders

--- src/core/database/models.ts
+++ src/core/database/models.ts
@@ -15,3 +15,21 @@ export const users = p.pgTable("users", {
     phoneNumber: p.text(),
     hashedPassword: p.text(),
 });
+
+export const folders = p.pgTable("folders", {
+    ...defaultModel,
+    name: p.text(),
+    creatorId: p.integer().references(() => users.id),
+});
+
+export const flashcards = p.pgTable("flashcards", {
+    ...defaultModel,
+    title: p.text(),
+    question: p.text(),
+    answer: p.text(),
+    folderId: p.integer().references(() => folders.id),
+    tags: p.text().array(),
+    difficulty: p.text(),
+    lastReviewed: p.timestamp(),
+    reviewCount: p.integer(),
+});
