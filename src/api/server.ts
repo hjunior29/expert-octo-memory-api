5a900c030ca1cfd515b02c197fbbf48c635f4fb9
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-03-15 13:18:43 -0300

    Add flashcard and folder routes to server configuration

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,9 +1,13 @@
+import { flashcardRoutes } from "$modules/flashcards/flashcard.routes";
+import { folderRoutes } from "$modules/folders/folder.routes";
 import { userRoutes } from "$modules/users/user.routes";
 
 export function startServer() {
 	const server = Bun.serve({
 		routes: {
 			...userRoutes,
+			...folderRoutes,
+			...flashcardRoutes,
 		},
 	});
 
